d6d8cb71c54b87e09d8c0862e09f72440b02e6c4
juancolchete <juanudkp@gmail.com>
2024-08-23 12:28:47 +0000

    refactor: positive and negative lookahead

--- regular-expressions/positive-and-negative-lookahead.js
+++ regular-expressions/positive-and-negative-lookahead.js
@@ -1,3 +1,3 @@
 let sampleWord = "astronaut";
-let pwRegex = /(?=\w{6,})(?=\D*\d{2,})/gi; // Change this line
+let pwRegex = /(?=\w{6,})(?=\D*\d{2,3})/; // Change this line
 let result = pwRegex.test(sampleWord);
